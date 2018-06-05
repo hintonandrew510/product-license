60fcdcf6aca6c0ea6d6ebfff83ae37fefeee2bf5
andrew hinton <hintonandrew510@gmail.com>
2018-06-04 20:56:50 -0400

    ad
    add

--- WebApplicationProductlicense/src/java/data/DatabaseManager.java
+++ WebApplicationProductlicense/src/java/data/DatabaseManager.java
@@ -16,6 +16,8 @@ Database: (create it using phpMyAdmin above)
     */
     // jbossewsalex
 	  private static String databaseURL = "jdbc:mysql://node71581-env-9265129.whelastic.net/license";// address
+          private static String databaseURLTest = "jdbc:mysql://sql3.freemysqlhosting.net/sql3241442";// address
+          
     private final static Logger LOGGER = Logger.getLogger(DatabaseManager.class
             .getName());
     // to the
@@ -26,8 +28,24 @@ Database: (create it using phpMyAdmin above)
         java.lang.Class.forName(driverName);
         StringBuilder builder = new StringBuilder();
         Connection connection = null;
-        connection = DriverManager.getConnection(databaseURL,
+        
+        
+       /*prod connection = DriverManager.getConnection(databaseURL,
                     "root", "OSCaki79649");
+        */
+        
+         
+        connection = DriverManager.getConnection(databaseURLTest,
+                    "sql3241442", "qAQVzm9Uiw");
+        
+        //sql3.freemysqlhosting.net	sql3241442	sql3241442
+        /*
+        sServer: sql3.freemysqlhosting.net
+        Name:    sql3241442
+       Username: sql3241442
+        Password: qAQVzm9Uiw
+        Port number: 3306
+        */
       
 
         return connection;
