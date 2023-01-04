3c6e5ea7698371a844537afef77921e451035318
hintonandrew510 <hintonandrew510@gmail.com>
2023-01-04 16:36:42 -0500

    Update DatabaseManager.java

--- webproductlicense/ProductLicense/src/java/data/DatabaseManager.java
+++ webproductlicense/ProductLicense/src/java/data/DatabaseManager.java
@@ -9,19 +9,13 @@ import java.util.logging.Logger;
 
 
 public class DatabaseManager {
-/*
-    Host: node71581-env-9265129.whelastic.net 
-Login: root 
-Password: OSCaki79649 
-Database: (create it using phpMyAdmin above)
-    */
-    // jbossewsalex
+
 	  private static String databaseURL = "jdbc:mysql://node71581-env-9265129.whelastic.net/license";// address
           private static String databaseURLTest = "jdbc:mysql://sql3.freemysqlhosting.net/sql3241442";// address
           private static String localDatabaseURL = "jdbc:mysql://localhost:3306/license?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC";
           private static String embeddedURL="jdbc:derby:sample";
           private static boolean isProduction = true;
-//https://node71581-env-9265129.whelastic.net/db_export.php?db=license
+
     public static boolean isIsProduction() {
         return isProduction;
     }
@@ -31,8 +25,7 @@ Database: (create it using phpMyAdmin above)
     }
     private final static Logger LOGGER = Logger.getLogger(DatabaseManager.class
             .getName());
-    // to the    sql3.freemysqlhosting.net/sql3241442 qAQVzm9Uiw
-    // database
+    
     private static String driverName = "com.mysql.jdbc.Driver";
      private static String localDriverName = "com.mysql.cj.jdbc.Driver";
 
@@ -42,49 +35,11 @@ Database: (create it using phpMyAdmin above)
         Connection connection = null;
         if (isProduction) {
              java.lang.Class.forName(driverName);
-            // connection = DriverManager.getConnection(databaseURL,
-             //      "root", "OSCaki79649");
+           
              connection = DriverManager.getConnection(localDatabaseURL,
                    "root", "password");
-        } else {
-             //java.lang.Class.forName("org.apache.derby.jdbc.ClientDriver");
-              java.lang.Class.forName(localDriverName);
-             connection = DriverManager.getConnection(localDatabaseURL,
-                   "root", "ahinton123");
-        }
+        } 
        
-        //connection = DriverManager.getConnection(databaseURLTest,
-                   // "sql3241442", "qAQVzm9Uiw");
-        
-       /*prod connection = DriverManager.getConnection(databaseURL,
-                    "root", "OSCaki79649");
-        
-        
-        To change payment method for the license manager application goto https://app.whelastic.net/
-
-
-logon with user name =hintonandrew510@gmail.com
-password = mike123
-        
-   phpadmin     https://node71581-env-9265129.whelastic.net/
-        */
-        
-         
-        /* test connection = DriverManager.getConnection(databaseURLTest,
-                    "sql3241442", "qAQVzm9Uiw");*/
-        
-        //sql3.freemysqlhosting.net	sql3241442	sql3241442
-        /*
-        sServer: sql3.freemysqlhosting.net
-        Name:    sql3241442
-       Username: sql3241442
-        Password: qAQVzm9Uiw
-        Port number: 3306
-        
-        jdbc:derby://localhost:1527/sample
-        */
-      
-
         return connection;
     }
     
