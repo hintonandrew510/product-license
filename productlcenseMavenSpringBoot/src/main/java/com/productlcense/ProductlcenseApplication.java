a079d37e0ba5238890e02b1237d373b982041e84
hintonandrew510 <hintonandrew510@gmail.com>
2025-08-27 14:50:06 -0400

    add

--- productlcenseMavenSpringBoot/src/main/java/com/productlcense/ProductlcenseApplication.java
+++ productlcenseMavenSpringBoot/src/main/java/com/productlcense/ProductlcenseApplication.java
@@ -2,12 +2,26 @@ package com.productlcense;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProductlcenseApplication {
+public class ProductlcenseApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductlcenseApplication.class, args);
-	}
+    @Override
+    // Configuring method has to be overridden    
+    protected SpringApplicationBuilder
+            configure(SpringApplicationBuilder application) {
+        return application.sources(
+                ProductlcenseApplication.class);
+    }
+
+    // Method 2    
+    // Main driver method    
+    public static void main(String[] args) {
+        SpringApplication.run(
+                ProductlcenseApplication.class,
+                args);
+    }
 
 }
---
+++ productlcenseMavenSpringBoot/src/main/resources/setenv.sh
@@ -0,0 +1,1 @@
+export JAVA_HOME="/Users/andrewhinton/Downloads/jdk-23.0.2.jdk/Contents/Home"
